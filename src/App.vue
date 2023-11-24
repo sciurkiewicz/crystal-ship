8066587722b53061e44aaae091d14608b61152b3
Szymon Ciurkiewicz <codeforgepl@gmail.com>
2023-11-24 15:26:10 +0100

    0.0.1

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,13 @@
 <script setup lang="ts">
+import LandingPage from './views/LandingPage.vue'
+
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <LandingPage></LandingPage>
   </div>
-  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-  Button
-</button>
+
 </template>
 
 <style scoped>
